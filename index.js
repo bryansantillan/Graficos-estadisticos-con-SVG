a71d24c50dea11510502c5487ed4a4cf053bf900
bryansantillan <49716323+bryansantillan@users.noreply.github.com>
2023-06-12 20:03:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 var videoEjemplo, reproducirVideo, barraVideo, progresoVideo, maximoVideo;
-var audioEjemplo, reproducirAudio, barraAudio, progresoAudio, maximoAudio;
 maximoVideo = 1240; //100% del video
-maximoAudio = 1240; //100% del video
 
 
 
 function comenzar() {
     // Almacenamos en variables los elementos HTML
     videoEjemplo = document.getElementById("videoEjemplo");
-    audioEjemplo = document.getElementById("audioEjemplo")
 
 
     reproducirVideo = document.getElementsByClassName("reproducir")[0];
     barraVideo = document.getElementsByClassName("barra")[0];
     progresoVideo = document.getElementsByClassName("progreso")[0];
 
-    reproducirAudio = document.getElementsByClassName("reproducir")[1];
-    barraAudio = document.getElementsByClassName("barra")[1];
-    progresoAudio = document.getElementsByClassName("progreso")[1];
     //eventos para cuando se pulse un boton...
     reproducirVideo.addEventListener("click", clicando, false);
     barraVideo.addEventListener("click", cambiando, false);
 
-    reproducirAudio.addEventListener("click", clicandoAudio, false);
-    barraAudio.addEventListener("click", cambiandoAudio, false);
 }
 
 function clicando() {
@@ -62,7 +54,7 @@ function cambiando(posicion) {
 
     videoEjemplo.currentTime = nuevoTiempo;
     var aux = ratonX - 2;//para que no se salga de la barra
-    progresoVideo.style.width = ratonX + px;
+    progresoVideo.style.width = ratonX + "px";
     // }
 }
 
